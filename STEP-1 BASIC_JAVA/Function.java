a1bce634825b50f8796cc49eaf770d25c5597268
myproject <khushidonda02@gmail.com>
2025-09-04 20:46:21 +0530

    Strivers-A2Z

--- STEP-1 BASIC_JAVA/Function.java
+++ STEP-1 BASIC_JAVA/Function.java
@@ -1,3 +1,22 @@
 public class Function {
-    
+    public static int maxSubArray(int[] arr) {
+        int mansum = Integer.MIN_VALUE;
+        int cursum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            cursum += arr[i];
+            if (cursum > mansum) {
+                mansum = cursum;
+            }
+            if (cursum < 0) {
+                cursum = 0;
+            }
+        }
+        return mansum; // ✅ added return
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-1, -2, -3};
+        System.out.println(maxSubArray(arr)); 
+    }
 }
